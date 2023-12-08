345c8a9a96c3abd9f24fddf9591bcb00688c0ee9
Igor Cotrim <igorcotrim.dev@gmail.com>
2023-12-08 19:39:35 -0300

    feat: add students repository and student entity properties

--- src/domain/forum/enterprise/entities/student/index.ts
+++ src/domain/forum/enterprise/entities/student/index.ts
@@ -2,9 +2,23 @@ import { Entity, UniqueEntityId } from '@/core/entities'
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudentProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
   static create(props: StudentProps, id?: UniqueEntityId) {
     const student = new Student(
       {
